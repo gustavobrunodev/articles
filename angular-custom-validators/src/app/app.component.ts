aa527dce1919dc2262f1b709e300900d6f5e9f97
gustavobgt <gustavo_bgt97@hotmail.com>
2024-04-17 11:37:30 -0300

    feat(angular-custom-validators): add creditcard custom validator

--- angular-custom-validators/src/app/app.component.ts
+++ angular-custom-validators/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CreditCardValidator } from './creditcardvalidator.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule, CreditCardValidator],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-custom-validators';
+  cardControl: FormControl = new FormControl(
+    '',
+    CreditCardValidator.validateCcNumber
+  );
+
+  displayData(value: any) {
+    alert(JSON.stringify(value));
+  }
 }
---
+++ angular-custom-validators/src/app/creditcardvalidator.directive.ts
@@ -0,0 +1,52 @@
+import { Directive } from '@angular/core';
+import {
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+  FormControl,
+  AbstractControl,
+} from '@angular/forms';
+
+@Directive({
+  standalone: true,
+  selector: '[validCreditCard]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: CreditCardValidator, multi: true },
+  ],
+})
+export class CreditCardValidator implements Validator {
+  // This is the implementation of the Validator interface
+  validate(c: FormControl): ValidationErrors | null {
+    // All we do is call the static method
+    return CreditCardValidator.validateCcNumber(c);
+  }
+
+  // This is the static method that does the actual validation
+  static validateCcNumber(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) {
+      return {
+        creditCard: '',
+      };
+    }
+
+    if (
+      !(
+        control.value.startsWith('37') ||
+        control.value.startsWith('4') ||
+        control.value.startsWith('5')
+      )
+    ) {
+      // Return error if card is not Amex, Visa or Mastercard
+      return {
+        creditCard:
+          'Your credit card number is not from a supported credit card provider',
+      };
+    } else if (control.value.length !== 16) {
+      console.log(control.value);
+      // Return error if length is not 16 digits
+      return { creditCard: 'A credit card number must be 16-digit long' };
+    }
+    // If no error, return null
+    return null;
+  }
+}
